88e5814fa13f69bf11c83d10c9144d0686ee27a2
Jack Hunsberger <jack.hunsberger2002@gmail.com>
2022-07-16 19:08:49 -0400

    it now kinda works

--- GameLoop.py
+++ GameLoop.py
@@ -4,6 +4,10 @@ from Tile import Tile
 from Player import Player
 
 
+def roll_dice():
+    return random.randrange(1, 7)
+
+
 class GameLoop:
     def __init__(self):
         self.WIDTH = 1280
@@ -12,17 +16,27 @@ class GameLoop:
         self.BG = pygame.image.load("assets/bg_sky__placeholder.jpg")
 
         self.BOARD = []
-        self.TILE_SPRITES = pygame.sprite.Group()
+        self.BOARD_SIZE = 100
         self.VISIBLE_SPRITES = pygame.sprite.Group()
         self.PLAYER = Player()
+        self.TURN_COUNT = 0
+        self.ANIMATING = False;
 
     def build_board(self):
         board = []
 
-        for i in range(0, 30):
+        tile = Tile()
+        tile.override_identity(False)
+        board.append(tile)
+
+        for i in range(0, self.BOARD_SIZE - 2):
             tile = Tile()
             board.append(tile)
-            self.TILE_SPRITES.add(tile)
+
+        for i in range(0, 10):
+            tile = Tile()
+            tile.override_identity(True)
+            board.append(tile)
 
         return board
 
@@ -33,7 +47,7 @@ class GameLoop:
 
         self.BOARD = self.build_board()
 
-    def render_screen(self):
+    def render_static_screen(self):
         screen = pygame.display.get_surface()
 
         # background (color is placeholder)
@@ -53,22 +67,66 @@ class GameLoop:
             x_val += 182
 
         self.VISIBLE_SPRITES.draw(screen)
+        self.VISIBLE_SPRITES.empty()
 
-    def run_game(self):
-        self.initialize()
+    def render_animating_screen(self):
+        screen = pygame.display.get_surface()
 
-        pygame.display.set_caption("TITLE")
+        # background (color is placeholder)
+        screen.blit(self.BG, (0, 0))
+
+        # draw each tile
+        x_val = 94
+        index = self.PLAYER.location
+
+        for i in range(0, 7):
+            tile = self.BOARD[index]
+            tile.rect.centerx = x_val
+
+        self.VISIBLE_SPRITES.draw(screen)
+
+    def start_combat(self):
+        pygame.display.set_caption("COMBAT")
 
         while True:
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        pass
+                        dice_result = roll_dice()
 
-            self.render_screen()
+    def run_game(self):
+        self.initialize()
+        pygame.display.set_caption("TITLE")
 
-            pygame.display.update()
+        while True:
+            if not self.ANIMATING:
+                # check current tile's identity:
+                # blank tile
+                if self.BOARD[self.PLAYER.location].identity == 2:
+                    pass
+                # gold tile
+                elif self.BOARD[self.PLAYER.location].identity == 0:
+                    pass
+                # combat tile
+                elif self.BOARD[self.PLAYER.location].identity == 1:
+                    pass
+
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                            dice_result = roll_dice()
+                            print("rolled a " + str(dice_result))
+                            print(self.PLAYER.location)
+                            self.PLAYER.location += dice_result
+
+                self.render_static_screen()
+
+                pygame.display.update()
+            else:
+                self.render_animating_screen()
--- Tile.py
+++ Tile.py
@@ -6,8 +6,8 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        self.identity = random.randrange(0, 4)
-        # blank tile
+        self.identity = random.randrange(0, 3)
+        # gold tile
         if self.identity == 0:
             self.image = pygame.image.load("assets/tile_placeholder1.jpg")
         # combat tile
@@ -27,3 +27,11 @@ class Tile(pygame.sprite.Sprite):
             pass
         elif self.identity == 1:
             pass
+
+    def override_identity(self, type):
+        if not type:
+            self.image = pygame.image.load("assets/tile_placeholder3.jpg")
+            self.identity = 2
+        else:
+            self.image = pygame.image.load("assets/tile_placeholder_end.jpg")
+            self.identity = -1
